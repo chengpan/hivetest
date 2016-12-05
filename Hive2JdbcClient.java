b6bc57c38aca916cf30d4b1c072ccefa3f92b467
chengpan <chengpandyx@126.com>
2016-12-05 16:25:08 +0800

    aa

--- Hive2JdbcClient.java
+++ Hive2JdbcClient.java
@@ -28,7 +28,13 @@ public class Hive2JdbcClient {
                         e.printStackTrace();
                         System.exit(1);
                 }
-
+                try {
+                        Class.forName(JDBC_DRIVER);
+                } catch (ClassNotFoundException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                        System.exit(1);
+                }
                 String[] colReg = getColumnsRegex();
                 //replace "hive" here with the name of the user the queries should run as
                 Connection con = DriverManager.getConnection("jdbc:hive2://10.9.96.4:10000/default", "root", "ucdnred@cat;;");
@@ -36,14 +42,67 @@ public class Hive2JdbcClient {
                 String tableName = "bigfile";
                 stmt.execute("drop table if exists " + tableName);
 
-
-
                 String logPath = "/hdtest/20161129/auc.tangdouimg.com/";
 
-                String tableColumns = "time string, clientIp string, method string, path string, query string, http_ver string";
+                String tableColumns = "time string" +
+								      ", clientIp string" +
+									  ", method string" +
+									  ", path string" +
+									  ", query string" +
+									  ", http_ver string" +
+									  ", hit_status int" +
+									  ", http_status int" +
+									  ", bytes_with_hdr bigint" +
+									  ", resp_delay int" +
+									  ", host_name string" +
+									  ", referer string" +
+									  ", user_name string" +
+									  ", back2src_method string" +
+									  ", back2src_host string" +
+									  ", server_ip string" +
+									  ", source_ip string" +
+									  ", source_ret_code string" + //warning!!! not int
+									  ", cli_req_end string" + //warning!!! not int
+									  ", src_req_end string" + //warning!!! not int
+									  ", user_agent string" +
+									  ", req_start_time int" +
+									  ", front_flow int" +
+									  ", range string" +
+									  ", bytes_no_hdr bigint" +
+									  ", file_bytes bigint" +
+									  ", cache_bytes bigint" +
+									  ", src_bytes bigint" +
+									  ", internal_err int" +
+									  ", finish_status int" +
+									  ", ufile_bytes bigint";
                 tableColumns = tableColumns + ", trail string";//trail
-
-                String regexStr = "\\\\[([0-9-: ]+)\\\\][\\\\s]+([0-9.]+)[\\\\s]+\\\"([^ ]+) ([^ ?]+)\\\\??([^ ]+)? ([^\\\"]+)\\\"";
+				String regexStr = "[\\\\s]*\\\\[([0-9-: ]+)\\\\]" + //time
+                       "[\\\\s]+([0-9.]+)" + //client ip
+                       "[\\\\s]+\\\"([^ ]+) ([^ ?]+)\\\\??([^ ]+)? ([^\\\"]+)\\\"" + // method + path + query + version
+                       "[\\\\s]+([^\\\\s]+)" + //hit status
+                       "[\\\\s]+([^\\\\s]+)" + //http status
+                       "[\\\\s]+([^\\\\s]+)" + //length
+                       "[\\\\s]+([^\\\\s]+)" + //response delay
+                       "[\\\\s]+([^\\\\s]+)" + //host
+                       "[\\\\s]+\\\"([^\\\"]*)\\\"" + //referer
+                       "[\\\\s]+([^\\\\s]+)" + //user name
+                       "[\\\\s]+([^/]+)/([^\\\\s]+)" + //source back method/source host 回源方式/域名
+                       "[\\\\s]+([^\\\\s]+)" + //server ip
+                       "[\\\\s]+([^\\\\s]+)" + //source ip
+                       "[\\\\s]+([^\\\\s]+)" + //source return code
+                       "[\\\\s]+([^\\\\s]+)" + //client req end mark 客户端请求结束标记
+                       "[\\\\s]+([^\\\\s]+)" + //source req end mark 源端请求结束标记
+                       "[\\\\s]+\\\"([^\\\"]*)\\\"" + //user agent
+                       "[\\\\s]+([^\\\\s]+)" + //request start time
+                       "[\\\\s]+([^\\\\s]+)" + //front end flow 前端flow
+                       "[\\\\s]+([^\\\\s]+)" + //range
+                       "[\\\\s]+([^\\\\s]+)" + //bytes sent (without http header)
+                       "[\\\\s]+([^\\\\s]+)" + //file bytes
+                       "[\\\\s]+([^\\\\s]+)" + //from cache bytes
+                       "[\\\\s]+([^\\\\s]+)" + //from source bytes
+                       "[\\\\s]+([^\\\\s]+)" + //internal error
+                       "[\\\\s]+([^\\\\s]+)" + //finish status
+                       "[\\\\s]+([^\\\\s]+)";  //from ufile size
                 regexStr = regexStr + "([\\\\s]+.*)?";//trail
 
                 String tableStr = String.format("create external table if not exists %s(%s)", tableName, tableColumns);
